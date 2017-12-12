78545ba12d8154f83ad95010ae8ac3ce6b9fdda1
Patrycja Bruździńska <patrycja.bruzdzinska@gmail.com>
2017-12-12 15:01:43 +0100

    Creating maps with signals sums.

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        Board board=new Board(100);
+//        System.out.println(board.checkPosition());
     }
 }
--- src/Robot.java
+++ src/Robot.java
@@ -1,10 +1,10 @@
 public class Robot {
     private Double pos_X;
     private Double pos_Y;
-    private Double signalA;
+    private Double signalA;     //power of receiving signal
     private Double signalB;
     private Double signalC;
-    private Double toA;
+    private Double toA;         //distance
     private Double toB;
     private Double toC;
     public Robot(Double x, Double y){
@@ -14,15 +14,18 @@ public class Robot {
 
     public void setToA(Double aX,Double aY) {
         this.toA = Math.sqrt(Math.pow(aX-pos_X,2)+Math.pow(aY-this.pos_Y,2));
-        this.signalA=-2*this.toA;
+//        this.signalA=-2*this.toA;
+        this.signalA=this.toA;
     }
     public void setToB(Double bX,Double bY) {
         this.toB = Math.sqrt(Math.pow(bX-pos_X,2)+Math.pow(bY-this.pos_Y,2));
-        this.signalB=-2*this.toB;
+//        this.signalB=-2*this.toB;
+        this.signalB=this.toB;
     }
     public void setToC(Double cX,Double cY) {
         this.toC = Math.sqrt(Math.pow(cX-pos_X,2)+Math.pow(cY-this.pos_Y,2));
-        this.signalC=-2*this.toC;
+//        this.signalC=-2*this.toC;
+        this.signalC=this.toC;
     }
 
     public Double getSignalA() {
