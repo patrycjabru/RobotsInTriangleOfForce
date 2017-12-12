aaa96ee071b3f1f17f031b50fd0bfcaf96b543cf
Patrycja Bruździńska <patrycja.bruzdzinska@gmail.com>
2017-12-12 15:01:26 +0100

    Creating maps with signals sums.

--- src/Board.java
+++ src/Board.java
@@ -1,6 +1,7 @@
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 public class Board {
@@ -12,6 +13,22 @@ public class Board {
     Transmitter A = new Transmitter();
     Transmitter B = new Transmitter();
     Transmitter C = new Transmitter();
+    Map<Integer, Double> signalsA;
+    Map<Integer, Double> signalsB;
+    Map<Integer, Double> signalsC;
+//    List<Double> sumAB = new ArrayList<>();
+//    List<Double> sumBC = new ArrayList<>();
+//    List<Double> sumCA = new ArrayList<>();
+//    List<Double> sumABC = new ArrayList<>();
+    Map<Integer, Double> sumAB;
+    Map<Integer, Double> sumAC;
+    Map<Integer, Double> sumCA;
+    Map<Integer, Double> sumABC;
+    Board(int numberOfRobots) {
+        createTransmitters();
+        createRobots(numberOfRobots);
+        sum();
+    }
     void createRobots(int number) {
         Random random=new Random();
         for (int i=0;i<number;i++) {
@@ -37,7 +54,27 @@ public class Board {
         B.setY(random.nextDouble()*this.y);
         C.setY(random.nextDouble()*this.y);
     }
+    void createSignalMaps() {
+        for (int i=0;i<robotList.size();i++) {
+            signalsA.put(i,robotList.get(i).getSignalA());
+            signalsB.put(i,robotList.get(i).getSignalB());
+            signalsC.put(i,robotList.get(i).getSignalC());
+        }
+    }
+    void sum() {
+//        for (int i=0;i<robotList.size();i++) {
+//            sumAB.add(robotList.get(i).getSignalA()+robotList.get(i).getSignalB());
+//            sumBC.add(robotList.get(i).getSignalB()+robotList.get(i).getSignalC());
+//            sumCA.add(robotList.get(i).getSignalC()+robotList.get(i).getSignalA());
+//            sumABC.add(robotList.get(i).getSignalA()+robotList.get(i).getSignalB()+robotList.get(i).getSignalC());
+//        }
+    }
     boolean checkPosition() {
+        System.out.println(mainRobot.getSignalA());
+        System.out.println(mainRobot.getSignalB());
+        System.out.println(mainRobot.getSignalC());
+//        if (mainRobot.getSignalA()+mainRobot.getSignalB()+mainRobot.getSignalC()>0)
+//            return true;
         return false;
     }
     public int getX() {
